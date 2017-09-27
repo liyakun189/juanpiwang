869396817ad65f84325b7032bcbed299466870c3
huzhangshuan <1300647703@qq.com>
2017-09-27 15:14:55 +0800

    youpinjianxuan

---
+++ src/pages/Choose.vue
@@ -0,0 +1,295 @@
+<template>
+  <div id="jpyx">
+    <!-- 头部两个按钮 -->
+     <div class="header">
+       <div>
+            <router-link to='/jpyx/sc' class="active">商城</router-link>
+       </div>
+       <div>
+            <router-link to='/jpyx/sht'>生活志</router-link>
+       </div>
+     </div> 
+     <!-- 搜索框 -->
+     <div class="search">
+       <a  href="https://m.juanpi.com/keywords?item=is_preferred">
+          <img src="https://goods2.juancdn.com/jas/170829/2/6/59a51122a9fcf87fea4afbe4_1080x144.png?imageMogr2/quality/85!/format/png"/>
+       </a>
+     </div>
+     <!-- banner 内容 -->
+     <div class="banner">
+       <a href="###">
+         <img src="https://goods5.juancdn.com/jas/170829/9/e/59a511f28150a17a085fcc8b_1080x400.png?iopcmd=convert&amp;Q=85&amp;dst=png"/>
+       </a>
+     </div>
+     <!-- 两个三个一行的商品 -->
+     <div class="goods-list">
+       <a href="http://web.juanpi.com/youxuan/category?cid=1859&amp;title=生活日用&amp;scene_key=yx_shry">
+          <img src="https://goods7.juancdn.com/jas/170830/d/d/59a6575da9fcf841480ab490_360x323.jpg?iopcmd=convert&amp;Q=85&amp;dst=jpg" style="width: 213.312px; height: 191.424px;"/>
+       </a>
+       <a href="http://web.juanpi.com/youxuan/category?cid=1860&amp;title=美妆个护&amp;scene_key=yx_mzgh">
+          <img src="https://goods5.juancdn.com/jas/170905/8/f/59ae2072a9fcf87ca46e7436_360x323.jpg?iopcmd=convert&amp;Q=85&amp;dst=jpg" style="width: 213.312px; height: 191.424px;">
+       </a>
+       <a href="http://web.juanpi.com/youxuan/category?cid=1855&amp;title=餐厨用具&amp;scene_key=yx_ccyj">
+         <img src="https://goods8.juancdn.com/jas/170905/f/f/59ae208ca9fcf87cdb4a6f8a_360x323.jpg?imageMogr2/quality/85!/format/jpg" style="width: 213.312px; height: 191.424px;">
+       </a>
+      </div>
+     <div class="goods-list">
+      <a href="http://web.juanpi.com/youxuan/category?cid=1856&amp;title=清洁收纳&amp;scene_key=yx_qjsn">
+        <img src="https://goods7.juancdn.com/jas/170830/c/3/59a6576ca9fcf841624c9283_360x323.jpg?iopcmd=convert&amp;Q=85&amp;dst=jpg" style="width: 213.312px; height: 191.424px;">
+      </a>
+      <a href="http://web.juanpi.com/youxuan/category?cid=1858&amp;title=服饰家纺&amp;scene_key=yx_fzjf">
+        <img src="https://goods3.juancdn.com/jas/170905/4/d/59ae20cfa9fcf87d6416b017_360x323.jpg?iopcmd=convert&amp;Q=85&amp;dst=jpg" style="width: 213.312px; height: 191.424px;">
+      </a>
+      <a href="http://web.juanpi.com/youxuan/category?cid=1857&amp;title=配饰箱包&amp;scene_key=yx_psxb">
+        <img src="https://goods5.juancdn.com/jas/170830/9/4/59a65d678150a11d52259e75_360x323.jpg?iopcmd=convert&amp;Q=85&amp;dst=jpg" style="width: 213.312px; height: 191.424px;">
+      </a>
+     </div>
+     <!-- 今日特价 -->
+     <div class="sale-today">
+       <span>今日特价</span>
+       <img class="sale_text" src="//jp.juancdn.com/jpwebapp_v1/images_v1/youxuan/sale.png">
+     </div>
+     <div class="sale-today-goods">
+        <ul>
+          <li>
+            <a href="https://web.juanpi.com/pintuan/shop/54117704">
+              <img src="https://goods5.juancdn.com/goods/170921/9/5/59c35ed4a9fcf85f8e53b7b2_800x800.png?iopcmd=thumbnail&amp;type=11&amp;height=310&amp;width=310" class="lazy">
+              <p class="price"><span class="RMB">¥</span><span>2.9</span></p>
+              <p class="text">轻便舒适情侣款凉拖鞋</p>
+            </a>
+          </li>
+          <li>
+            <a href="https://web.juanpi.com/pintuan/shop/54017324">
+              <img src="https://goods4.juancdn.com/bao/170608/6/e/5938e8b4a43d1f1cba4bdffc_800x800.jpg?imageMogr2/thumbnail/310x310!" class="lazy">
+              <p class="price"><span class="RMB">¥</span><span>1.9</span></p>
+              <p class="text">纯棉防滑船袜(2双)</p>
+            </a>
+          </li>
+          <li>
+            <a href="https://web.juanpi.com/pintuan/shop/55807054">
+              <img src="https://goods5.juancdn.com/bao/170614/8/b/5940db93ad0a49b1398b45c5_800x800.jpg?iopcmd=thumbnail&amp;type=11&amp;height=310&amp;width=310" class="lazy">
+              <p class="price"><span class="RMB">¥</span><span>0.9</span></p>
+              <p class="text">防紫外线冰丝袖套2双</p>
+            </a>
+          </li>
+        </ul>
+     </div>
+     <div class="sale-today">
+       <span>人气推荐</span>
+       <button>查看更多>></button>
+     </div>
+     <div class="sale-today-goods">
+        <ul>
+          <li>
+            <a href="https://web.juanpi.com/pintuan/shop/54117704">
+              <img src="https://goods5.juancdn.com/goods/170921/9/5/59c35ed4a9fcf85f8e53b7b2_800x800.png?iopcmd=thumbnail&amp;type=11&amp;height=310&amp;width=310" class="lazy">
+              <p class="price"><span class="RMB">¥</span><span>2.9</span></p>
+              <p class="text">轻便舒适情侣款凉拖鞋</p>
+            </a>
+          </li>
+          <li>
+            <a href="https://web.juanpi.com/pintuan/shop/54017324">
+              <img src="https://goods4.juancdn.com/bao/170608/6/e/5938e8b4a43d1f1cba4bdffc_800x800.jpg?imageMogr2/thumbnail/310x310!" class="lazy">
+              <p class="price"><span class="RMB">¥</span><span>1.9</span></p>
+              <p class="text">纯棉防滑船袜(2双)</p>
+            </a>
+          </li>
+          <li>
+            <a href="https://web.juanpi.com/pintuan/shop/55807054">
+              <img src="https://goods5.juancdn.com/bao/170614/8/b/5940db93ad0a49b1398b45c5_800x800.jpg?iopcmd=thumbnail&amp;type=11&amp;height=310&amp;width=310" class="lazy">
+              <p class="price"><span class="RMB">¥</span><span>0.9</span></p>
+              <p class="text">防紫外线冰丝袖套2双</p>
+            </a>
+          </li>
+        </ul>
+     </div>
+     <!-- <routerview></routerview>  -->
+  </div>
+</template>
+    
+<script>
+export default {
+  name: "jpyx",
+  data () {
+    return {
+        
+    };
+  }
+}
+
+!function(e,t){"use strict";var i={isIPhone:/iphone/gi.test(e.navigator.appVersion),isAndroid:/android/gi.test(e.navigator.appVersion),isApp:t.cookie.indexOf("qm_device_id")!==-1};i.isAppNotWK=i.isApp&&t.cookie.indexOf("isWKWebView=1")===-1,function(){var n=t.querySelector('meta[name="viewport"]'),r=t.querySelector('meta[name="hotcss"]'),d=e.devicePixelRatio||1,a=540,s=0;if(d=d>=3?3:d>=2?2:1,i.isAndroid&&(d=1),r){var o=r.getAttribute("content");if(o){var m=o.match(/initial-dpr=([d.]+)/);m&&(d=parseFloat(m[1]));var c=o.match(/max-width=([d.]+)/);c&&(a=parseFloat(c[1]));var u=o.match(/design-width=([d.]+)/);u&&(s=parseFloat(u[1]))}}t.documentElement.setAttribute("data-dpr",d),i.dpr=d,t.documentElement.setAttribute("max-width",a),i.maxWidth=a,s&&t.documentElement.setAttribute("design-width",s),i.designWidth=s;var p=1/d;i.isIPhone&&375==e.screen.width&&i.isAppNotWK&&(p=.48);var l="width=device-width, initial-scale="+p+", minimum-scale="+p+", maximum-scale="+p+", user-scalable=no";n?n.setAttribute("content",l):(n=t.createElement("meta"),n.setAttribute("name","viewport"),n.setAttribute("content",l),t.head.appendChild(n))}(),i.px2rem=function(e,t){return t||(t=parseInt(i.designWidth,10)),320*parseInt(e,10)/t/20},i.rem2px=function(e,t){return t||(t=parseInt(i.designWidth,10)),20*e*t/320},i.remAddpx=function(e,t,n){var r=i.rem2px(e,n),d=t*i.dpr/2;return r+d},i.mresize=function(){var n=t.documentElement.getBoundingClientRect().width||e.innerWidth;return i.maxWidth&&n/i.dpr>i.maxWidth&&(n=i.maxWidth*i.dpr),!!n&&(t.documentElement.style.fontSize=20*n/320+"px",void(i.callback&&i.callback()))},i.mresize(),e.addEventListener("resize",function(){clearTimeout(i.tid),i.tid=setTimeout(i.mresize,33)},!1),e.addEventListener("load",i.mresize,!1),setTimeout(function(){i.mresize()},333),e.hotcss=i}(window,document);
+        
+</script>
+    
+<style lang="css" scoped>
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing:border-box;
+  }
+  html{
+    font-size: 40px;
+  }
+  #jpyx{
+    width:100%;
+    height: 100%;
+  }
+  .header{
+    width: 100%;
+    height: 1.70667rem;
+    line-height: 1.70667rem;
+    text-align: center;
+    font-size: .68267rem;
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 9999;
+    border-bottom: .04267rem solid #ebebeb
+  }
+  .header div{
+    float: left;
+    width: 50%;
+  }
+  .header div router-link{
+    display: inline-block;
+    width: auto;
+    height: 1.70667rem;
+  }
+  .active{
+    color: #e66b00;
+    border-bottom: .08533rem solid #e66b00;
+  }
+  .search{
+    border-bottom: .02133rem solid #ebebeb;
+    border-top: .02133rem solid #ebebeb;
+    clear: both;
+    position: relative;
+    margin-top: 1.70667rem;
+    width: 100%;
+    height:2.125rem; 
+  }
+  .search a{
+    display: inline-block;
+    width: 100%;
+    height: 2.125rem;
+    overflow: hidden;
+    text-decoration: none;
+  }
+  .search img{
+    height: 2.125rem;
+    width: 100%;
+  }
+  .banner{
+    width: 100%;
+    height: 6.15rem;
+    overflow: hidden;
+  }
+  .banner img{
+      width: 100%;
+    height: 6.15rem;
+  }
+  .goods-list{
+    width: 100%;
+    overflow: hidden;
+  }
+  .goods-list a{
+    text-decoration: none;
+  }
+  .goods-list a img{
+    float: left;
+  }
+  .sale-today{
+    width: 100%;
+    height: 2.048rem;
+    line-height: 2.048rem;
+    margin-top: .42667rem;
+    padding-left: .59733rem;
+    box-sizing: border-box;
+  }
+  .sale-today span{
+    font-size: .68267rem;
+    color: #4a4a4a;
+    margin-left: .17067rem;
+    width: 1.87733rem;
+    height: .64rem;
+    line-height: 2.048rem;
+  }
+  .sale-today img{
+    margin-left: .17067rem;
+    width: 1.87733rem;
+    height: .64rem;
+    vertical-align: middle;
+  }
+  .sale-today button{
+    float: right;
+    background: none;
+    font-size: 0.45rem;
+    border: none;
+    margin-right:0.25rem;
+    color: gray;
+    line-height: 2.048rem; 
+  }
+  .sale-today-goods{
+    width: 100%;
+    display: block;
+    height: 7.808rem;
+    background-color: #fff;
+    overflow: hidden;
+    margin-bottom: .42667rem;
+  }
+  .sale-today-goods ul{
+    display: block;
+    width: auto;
+    margin-top: 0;
+    height: 100%;
+    white-space: nowrap;
+    padding: 0 .42667rem;
+    overflow-x: auto;
+    overflow-y: hidden;
+    -webkit-overflow-scrolling: none;
+  }
+  .sale-today-goods ul li{
+        vertical-align: top;
+    display: inline-block;
+    width: 5.54667rem;
+    height: 100%;
+    margin-right: .42667rem;
+    text-align: center;
+    background-color: #fff;
+    margin-top: 0;
+    list-style: none;
+  }
+  .sale-today-goods ul li a{
+    display: inline-block;
+    width: 5.54667rem;
+    height: 7.808rem;
+    overflow: hidden;
+    text-decoration: none;
+  }
+  .sale-today-goods img{
+    display: block;
+    width: 5.54667rem;
+    height: 5.504rem;
+    margin: 0;
+    background: url(//jp.juancdn.com/jpwebapp_v1/images_v1/youxuan/loading.png) no-repeat 50%;
+    background-size: 100% auto;
+  }
+  .sale-today-goods a .price {
+    width: 100%;
+    line-height: .768rem;
+    height: .768rem;
+    font-size: .59733rem;
+    color: #e66b01;
+    margin-top: .256rem;
+}
+.sale-today-goods a .text {
+    width: 100%;
+    font-size: .512rem;
+    color: #4a4a4a;
+    line-height: .72533rem;
+    height: .72533rem;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    white-space: nowrap;
+}
+</style>
